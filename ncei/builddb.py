495daa61903c8e7c514424016e01d333ca649176
bkatiemills <you@example.com>
2025-06-18 21:40:49 -0400

    persist the qc flags from the NCEI run in the DB

--- ncei/builddb.py
+++ ncei/builddb.py
@@ -12,12 +12,21 @@ for file in glob.glob(sys.argv[1]+"/*"):
     fields_to_extract = ['time', 'date']
     filetype = file.split('_')[1]
 
-    n_records = len(ncfile.dimensions['casts'])  # or your record dimension
+    n_records = len(ncfile.dimensions['casts'])
+    temperature_levels = ncfile.variables['Temperature_row_size']
+    iquod_flags = ncfile.variables['Temperature_IQUODflag']
+    levelindex = 0
     for i in range(n_records):
         doc = {}
         doc['geolocation'] = {"type": "Point", "coordinates": [float(ncfile.variables['lon'][i]), float(ncfile.variables['lat'][i])]}
         doc['_id'] = int(ncfile.variables['wod_unique_cast'][i])
         doc['filetype'] = filetype
+        try:
+            nlevels = int(temperature_levels[i])
+        except:
+            nlevels = 0 # sometimes this number is masked instead of just being 0
+        doc['temperature_qc_flags'] = iquod_flags[levelindex:levelindex+nlevels].filled(None).tolist()
+        levelindex += nlevels
         for field in fields_to_extract:
             var = ncfile.variables[field]
             if var.ndim == 1:
@@ -27,5 +36,7 @@ for file in glob.glob(sys.argv[1]+"/*"):
 
         collection.insert_one(doc)
 
-
 collection.create_index([("geolocation", "2dsphere")])
+
+
+
